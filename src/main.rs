4931eb59202c863cfa07bbc68a257a6792c8f4c2
Abhishek Sathiabalan <asathiabalan@gmail.com>
2023-05-25 11:18:22 -0400

    Added clap argument parsing. Added ability to do math directly from stdin, instead of just files

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,73 @@
+use clap::Parser;
 use evaluator::evaluate;
 
 use crate::parser::parse;
 
-use std::error::Error;
+use std::error::{Error};
 use std::fs::File;
+use std::io::{self, Write};
 use std::io::Read;
-use std::process;
 use std::result::Result;
+use std::time::{Instant};
 
 mod evaluator;
 mod parser;
 mod types;
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let mut test_file = File::open("test.bc")?;
-    let mut input_file_contents = String::new();
-    test_file.read_to_string(&mut input_file_contents)?;
-    for line in input_file_contents.lines().by_ref() {
-        if line.is_empty() {
-            continue;
-        }
-        match calculate(line) {
-            Ok(result) => println!("{:?}", result),
-            Err(error) => eprintln!("{:?}", error)
-        }
+#[derive(Parser,Default,Debug)]
+#[clap(version, about="A simple calculator. Written by Abhishek Sathiabalan & Balaji Sivaraman. MIT license.")]
+struct Arguments {
+    /// file to parse against
+    #[clap(short, long)]
+    file: Option<String>
+}
 
-    }
+fn main() -> Result<(), Box<dyn Error>> {
+    let args = Arguments::parse();
+    match args.file {
+        Some(path) => {
+            let mut test_file = File::open(path)?;
+            let mut input_file_contents = String::new();
+            test_file.read_to_string(&mut input_file_contents)?;
+            for line in input_file_contents.lines().by_ref() {
+                process(line);
+            }
+        }
+        None => {
+            let stdin = io::stdin(); 
+            loop {
+                print!(">");
+                io::stdout().flush()?;
+                let mut user_input = String::new();
+                stdin.read_line(&mut user_input)?;
+                process(&user_input);
+            }
+        }
+    }    
     Ok(())
 }
 
-fn calculate(input: &str) -> Result<f64, Box<dyn Error>> {
-    let (input, parsed_line) = parse(input).unwrap();
-    println!("{:?}", parsed_line);
-    if !input.is_empty() {
-        eprintln!("parsing error, input remaining {:?}", input);
-        process::exit(1);
+fn process(line: &str) {
+    if line.is_empty() {
+        return;
     }
-    let result = evaluate(parsed_line);
-    return Ok(result);
+    let start = Instant::now();
+    match calculate(line) {
+        Ok(result) => {
+            let duration = start.elapsed();
+            println!("{:?} ({:?})", result, duration)
+        },
+        Err(error) => eprintln!("{:?}", error)
+    }
+}
+
+fn calculate(input: &str) -> Result<f64, String> {
+    match parse(input) {
+        Ok( expr ) => {
+            let (_, parsed_line) = expr;
+            let result = evaluate(parsed_line);
+            Ok(result)
+        }
+        Err(error) => Err(error.to_string())
+    }
 }
